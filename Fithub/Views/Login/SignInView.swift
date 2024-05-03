d65f6ae2defaa0740fe575930672af3b1fcb5fa6
Chalisa Thummaraj <6410685041@student.tu.ac.th>
2024-05-03 11:43:51 +0700

    UI SignIn and Signup

--- Fithub/Views/Login/SignInView.swift
+++ Fithub/Views/Login/SignInView.swift
@@ -13,33 +13,121 @@ struct SignInView: View {
     
     var body: some View {
         NavigationView {
-            VStack {
-                Text("FitHub")
-
-                Form {
-                    TextField("Email Address", text: $viewModel.email)
-                        .textFieldStyle(DefaultTextFieldStyle())
-                        .autocorrectionDisabled()
-                        .autocapitalization(.none)
-                    SecureField("Password", text: $viewModel.password)
-                        .textFieldStyle(DefaultTextFieldStyle())
-
-                    Button("Login") {
-                        viewModel.login()
+            ZStack{
+                VStack{
+                    Image("background2")
+                        .ignoresSafeArea(.all)
+                    Spacer()
+                }
+                
+                VStack{
+                    Spacer()
+                        .frame(height: 430)
+                    Rectangle()
+                        .foregroundColor(Color(UIColor(hex: "#292929")))
+                        .rotationEffect(Angle(degrees: 250))
+                        .frame(width: 700, height: 700)
+                }
+                
+                VStack{
+                    Rectangle()
+                        .foregroundColor(Color(UIColor(hex: "#164EAD")))
+                        .rotationEffect(Angle(degrees: 250))
+                        .frame(width: 40, height: 700)
+                    
+                    Spacer()
+                        .frame(height: 440)
+                }
+                
+                VStack{
+                    HStack{
+                        Image(systemName: "dumbbell.fill")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .foregroundColor(Color(UIColor(hex: "#2B70E4")))
+                            .frame(width: 70)
+                        Text("FitHub")
+                            .foregroundColor(.white)
+                            .font(.system(size: 50))
+                            .bold()
+                        Spacer()
+                            .frame(width: 80)
                     }
-
+                    Spacer()
+                        .frame(height: 650)
+                        
+                }
+                
+                VStack {
+                    Spacer()
+                        .frame(height: 200)
+                    Text("SIGN IN")
+                        .foregroundColor(.white)
+                        .font(.system(size: 40))
+                        .bold()
+                        .padding(.bottom)
+                    VStack(alignment: .leading) {
+                        Text("Email")
+                            .foregroundColor(.white)
+                            .padding(.horizontal)
+                        TextField("Email Address", text: $viewModel.email)
+                            .autocorrectionDisabled()
+                            .autocapitalization(.none)
+                            .foregroundColor(.white)
+                            .font(.system(size: 20))
+                            .padding(EdgeInsets(top: 8, leading: 20, bottom: 8, trailing: 20))
+                            .background(Color(UIColor(hex: "#8399BD")))
+                            .cornerRadius(25)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 25)
+                                    .stroke(Color(UIColor(hex: "#8399BD")), lineWidth: 1)
+                            )
+                            .padding([.horizontal, .bottom])
+                        
+                        Text("Password")
+                            .foregroundColor(.white)
+                            .padding(.horizontal)
+                        SecureField("Password", text: $viewModel.password)
+                            .autocorrectionDisabled()
+                            .autocapitalization(.none)
+                            .foregroundColor(.white)
+                            .font(.system(size: 20))
+                            .padding(EdgeInsets(top: 8, leading: 20, bottom: 8, trailing: 20))
+                            .background(Color(UIColor(hex: "#8399BD")))
+                            .cornerRadius(25)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 25)
+                                    .stroke(Color(UIColor(hex: "#8399BD")), lineWidth: 1)
+                            )
+                            .padding([.horizontal, .bottom])
+                    }
+                    .padding(.horizontal,190)
+                    .padding(.bottom)
+                    
+                    Button{
+                        viewModel.login()
+                    } label: {
+                        ZStack(alignment: .center){
+                            RoundedRectangle(cornerRadius: 20)
+                                .foregroundColor(Color(UIColor(hex: "#164EAD")))
+                                .frame(width: 200, height: 50)
+                            Text("Log in")
+                                .foregroundColor(.white)
+                        }
+                    }
+                    
+                    
                     if !viewModel.errorMessage.isEmpty {
                         Text(viewModel.errorMessage)
                             .foregroundColor(.red)
                     }
+                    
+                    VStack {
+                        NavigationLink("Create an Account", destination: SignUpView())
+                    }
+                    
+                    NavigationLink("", destination: MainView(), isActive: $viewModel.isLoggedIn)
                 }
-
-                VStack {
-                    Text("New Around Here")
-                    NavigationLink("Create an Account", destination: SignUpView())
-                }
-
-                NavigationLink("", destination: MainView(), isActive: $viewModel.isLoggedIn)
             }
         }
     }
--- Fithub/Views/Login/SignUpView.swift
+++ Fithub/Views/Login/SignUpView.swift
@@ -9,45 +9,223 @@ import SwiftUI
 
 struct SignUpView: View {
     @StateObject var viewModel = SignUpViewModel()
+    @State var showSecondPage = false
     
     var body: some View {
-        VStack {
-            Text("Register")
-            Form {
-                TextField("Full Name", text: $viewModel.name)
-                    .textFieldStyle(DefaultTextFieldStyle())
+        ZStack{
+            if (!showSecondPage){
+                // first page
+                VStack{
+                    Image("background2")
+                        .ignoresSafeArea(.all)
+                    Spacer()
+                }
                 
-                TextField("Email Address", text: $viewModel.email)
-                    .textFieldStyle(DefaultTextFieldStyle())
-                    .autocorrectionDisabled()
-                    .autocapitalization(.none)
+                VStack{
+                    Spacer()
+                        .frame(height: 430)
+                    Rectangle()
+                        .foregroundColor(Color(UIColor(hex: "#292929")))
+                        .rotationEffect(Angle(degrees: 250))
+                        .frame(width: 700, height: 700)
+                }
                 
-                SecureField("Password", text: $viewModel.password)
-                    .textFieldStyle(DefaultTextFieldStyle())
+                VStack{
+                    Rectangle()
+                        .foregroundColor(Color(UIColor(hex: "#164EAD")))
+                        .rotationEffect(Angle(degrees: 250))
+                        .frame(width: 40, height: 700)
+                    
+                    Spacer()
+                        .frame(height: 440)
+                }
                 
-                TextField("Weight", value: $viewModel.weight, formatter: NumberFormatter())
-                    .textFieldStyle(DefaultTextFieldStyle())
+                VStack{
+                    HStack{
+                        Image(systemName: "dumbbell.fill")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .foregroundColor(Color(UIColor(hex: "#2B70E4")))
+                            .frame(width: 70)
+                        Text("FitHub")
+                            .foregroundColor(.white)
+                            .font(.system(size: 50))
+                            .bold()
+                        Spacer()
+                            .frame(width: 80)
+                    }
+                    Spacer()
+                        .frame(height: 650)
+                    
+                }
                 
-                TextField("Height", value: $viewModel.height, formatter: NumberFormatter())
-                    .textFieldStyle(DefaultTextFieldStyle())
+                VStack {
+                    Spacer()
+                        .frame(height: 140)
+                    Text("Create")
+                        .foregroundColor(.white)
+                        .font(.system(size: 40))
+                        .bold()
+                    Text("New Account")
+                        .foregroundColor(.white)
+                        .font(.system(size: 40))
+                        .bold()
+                        .padding(.bottom)
+                    
+                    VStack {
+                        TextField("Email Address", text: $viewModel.email)
+                            .autocorrectionDisabled()
+                            .autocapitalization(.none)
+                            .foregroundColor(.white)
+                            .font(.system(size: 20))
+                            .padding(EdgeInsets(top: 8, leading: 20, bottom: 8, trailing: 20))
+                            .background(Color(UIColor(hex: "#8399BD")))
+                            .cornerRadius(25)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 25)
+                                    .stroke(Color(UIColor(hex: "#8399BD")), lineWidth: 1)
+                            )
+                            .padding([.horizontal, .bottom])
+                        
+                        SecureField("Password", text: $viewModel.password)
+                            .autocorrectionDisabled()
+                            .autocapitalization(.none)
+                            .foregroundColor(.white)
+                            .font(.system(size: 20))
+                            .padding(EdgeInsets(top: 8, leading: 20, bottom: 8, trailing: 20))
+                            .background(Color(UIColor(hex: "#8399BD")))
+                            .cornerRadius(25)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 25)
+                                    .stroke(Color(UIColor(hex: "#8399BD")), lineWidth: 1)
+                            )
+                            .padding([.horizontal, .bottom])
+                        
+                        
+                        Button{
+                            showSecondPage = true
+                        } label: {
+                            ZStack(alignment: .center){
+                                RoundedRectangle(cornerRadius: 20)
+                                    .foregroundColor(Color(UIColor(hex: "#164EAD")))
+                                    .frame(width: 200, height: 50)
+                                Text("Next")
+                                    .foregroundColor(.white)
+                            }
+                        }
+                    } 
+                    .padding(.horizontal,190)
+                    .padding(.bottom)
+                }
+            } else {
+                // second page
+                VStack{
+                    Image("background2")
+                        .ignoresSafeArea(.all)
+                    Spacer()
+                }
                 
-                Text("Gender")
-                Picker("Gender", selection: $viewModel.gender) {
-                    ForEach(Gender.allCases, id: \.self) { gender in
-                        Text(gender.displayName)
+                VStack{
+                    Spacer()
+                        .frame(height: 100)
+                    Rectangle()
+                        .foregroundColor(Color(UIColor(hex: "#292929")))
+                        .frame(width: .infinity, height: .infinity)
+                }
+                
+                VStack {
+                    Spacer()
+                        .frame(height: 50)
+                    Text("Information")
+                        .foregroundColor(.white)
+                        .font(.system(size: 40))
+                        .bold()
+                        .padding(.bottom)
+                    
+                    ScrollView{
+                        VStack{
+                            TextField("Full Name", text: $viewModel.name)
+                                .autocorrectionDisabled()
+                                .autocapitalization(.none)
+                                .foregroundColor(.white)
+                                .font(.system(size: 20))
+                                .padding(EdgeInsets(top: 8, leading: 20, bottom: 8, trailing: 20))
+                                .background(Color(UIColor(hex: "#8399BD")))
+                                .cornerRadius(25)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 25)
+                                        .stroke(Color(UIColor(hex: "#8399BD")), lineWidth: 1)
+                                )
+                                .padding([.horizontal, .bottom])
+                            
+                            HStack{
+                                VStack{
+                                    Text("Weight")
+                                    TextField("Weight", value: $viewModel.weight, formatter: NumberFormatter())
+                                        .autocorrectionDisabled()
+                                        .autocapitalization(.none)
+                                        .foregroundColor(.white)
+                                        .font(.system(size: 20))
+                                        .padding(EdgeInsets(top: 8, leading: 20, bottom: 8, trailing: 20))
+                                        .background(Color(UIColor(hex: "#8399BD")))
+                                        .cornerRadius(25)
+                                        .overlay(
+                                            RoundedRectangle(cornerRadius: 25)
+                                                .stroke(Color(UIColor(hex: "#8399BD")), lineWidth: 1)
+                                        )
+                                        .padding(.horizontal)
+                                }
+                                VStack{
+                                    Text("Height")
+                                    TextField("Height", value: $viewModel.height, formatter: NumberFormatter())
+                                        .autocorrectionDisabled()
+                                        .autocapitalization(.none)
+                                        .foregroundColor(.white)
+                                        .font(.system(size: 20))
+                                        .padding(EdgeInsets(top: 8, leading: 20, bottom: 8, trailing: 20))
+                                        .background(Color(UIColor(hex: "#8399BD")))
+                                        .cornerRadius(25)
+                                        .overlay(
+                                            RoundedRectangle(cornerRadius: 25)
+                                                .stroke(Color(UIColor(hex: "#8399BD")), lineWidth: 1)
+                                        )
+                                        .padding(.horizontal)
+                                }
+                            }
+                            .foregroundColor(.white)
+                            
+                            HStack{
+                                Text("Gender")
+                                    .foregroundColor(.white)
+                                Picker("Gender", selection: $viewModel.gender) {
+                                    ForEach(Gender.allCases, id: \.self) { gender in
+                                        Text(gender.displayName)
+                                    }
+                                }
+                            }
+                            
+                            Text("Select birthdate")
+                                .foregroundColor(.white)
+                            DatePicker("Birthdate", selection: $viewModel.birthdate, displayedComponents: .date)
+                                .datePickerStyle(GraphicalDatePickerStyle())
+                            
+                        }
+                        .padding(.horizontal,70)
+                        
+                        Button{
+                            viewModel.register()
+                        } label: {
+                            ZStack(alignment: .center){
+                                RoundedRectangle(cornerRadius: 20)
+                                    .foregroundColor(Color(UIColor(hex: "#164EAD")))
+                                    .frame(width: 200, height: 50)
+                                Text("Sign up")
+                                    .foregroundColor(.white)
+                            }
+                        }
                     }
                 }
-                
-                DatePicker("Birthdate", selection: $viewModel.birthdate)
-                    .datePickerStyle(GraphicalDatePickerStyle())
-                
-                Button("Create Account"){
-                    viewModel.register()
-                }
             }
-            .offset(y: -50)
-            
-            Spacer()
         }
     }
 }
